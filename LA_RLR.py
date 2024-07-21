b51242e8011b6f2cf8aa033485c799a564584968
vince013 <82330912+vince013@users.noreply.github.com>
2024-07-21 13:59:22 +0200

    Init

---
+++ LA_RLR.py
@@ -0,0 +1,54 @@
+import turtle
+def langton():
+    window = turtle.Screen()
+    window.bgcolor('white')
+    window.screensize(2500, 2500)
+    window.tracer(0,0)
+
+    ant = turtle.Turtle()
+    ant.shape('square')
+    ant.shapesize(0.5)
+    ant.speed(0)
+
+    def coordinate(ant):
+        return (round(ant.xcor()), round(ant.ycor()))
+
+    def invert(field, ant, color):
+        field[coordinate(ant)] = color
+
+    #counterMax anpassen für mehr/weniger Schritte
+    rule, colors = "RLR", ["white", "black", "red"]  
+    fields, pos, step = {}, coordinate(ant), 10
+    degree, counter, counterMax = 90, 0, 25000
+    
+    while counter!=counterMax:
+        if pos not in fields:
+            current_color = "white"
+        else:
+            current_color = fields[pos]
+    
+        color_index = colors.index(current_color)
+        
+        new_color = colors[(color_index + 1) % len(colors)]
+        turn_direction = rule[color_index]
+        
+        ant.fillcolor(new_color)
+        invert(fields, ant, new_color)
+        ant.stamp()
+        
+        if turn_direction == "R":
+            ant.right(degree)
+        else:
+            ant.left(degree)
+        
+        ant.forward(step)
+        pos = coordinate(ant)
+        counter += 1
+    
+        if counter % 1000 == 0:
+            window.update()
+            
+    window.update()
+    window.mainloop()
+
+langton()
